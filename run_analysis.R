149704610368c6e7fbdc0668f2b8a93bbe121ff7
Damian S <damian77504@wp.pl>
2021-09-28 17:09:24 +0200

    my project commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-
+#Getting and Cleaning Project Assignment
 #Data source:
 # A full description is available at the site where the data was obtained:
 #                http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
